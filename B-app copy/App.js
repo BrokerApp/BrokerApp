4b9e157b4c426daf01d56d633dd31438f510055b
Yeghishe Karapetyan <yeghishe.karapetyan3@gmail.com>
2022-06-04 22:57:31 +0400

    Co-authored-by: Suren Petrosyan <suren_petrosyan@edu.aua.am>

--- B-app copy/App.js
+++ B-app copy/App.js
@@ -1,7 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "space-between", alignItems: "center", backgroundColor: "#eb4034", flexDirection: "column" }} >
+        <StatusBar barStyle="light-content" backgroundColor="#191914" />
+        <View style={{ height: 50, }} />
+        <Image source={require("./assets/icon.png")} style={{ width: 150, height: 150, }} />
+
+        <View style={{ justifyContent: "flex-end" }} >
+          <Text style={{ marginRight: 11, backgroundColor: "#eb4034", color: "black", borderRadius: 11, fontSize: 28, marginBottom: 20 }}> finfriend </Text>
+        </View>
+      </View>
+    )
+  }
+
+
   return (
     <View style={styles.container}>
       <Text>Text 2</Text>
@@ -18,3 +42,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ B-app copy/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASAYscilSX9sfF53pC6aAJuQX-JTpJt_s",
+    authDomain: "broker-app-202ae.firebaseapp.com",
+    projectId: "broker-app-202ae",
+    storageBucket: "broker-app-202ae.appspot.com",
+    messagingSenderId: "785510833280",
+    appId: "1:785510833280:web:97690d4c3a1eb0c7c5da65",
+    measurementId: "G-KBC7G0PMBC"
+};
+
+const app = initializeApp(firebaseConfig);

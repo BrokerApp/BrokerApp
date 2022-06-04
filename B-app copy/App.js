06455a2c537b77bc61c97e6dec138c05e9ae650a
Yeghishe Karapetyan <yeghishe.karapetyan3@gmail.com>
2022-06-04 23:02:19 +0400

    s

--- B-app copy/App.js
+++ B-app copy/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
@@ -8,15 +8,14 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false)
-    }, 1000)
+    }, 10000)
   }, [])
 
   if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: "space-between", alignItems: "center", backgroundColor: "#eb4034", flexDirection: "column" }} >
-        <StatusBar barStyle="light-content" backgroundColor="#191914" />
         <View style={{ height: 50, }} />
-        <Image source={require("./assets/icon.png")} style={{ width: 150, height: 150, }} />
+        {/* <Image source={require("./assets/icon.png")} style={{ width: 150, height: 150, }} /> */}
 
         <View style={{ justifyContent: "flex-end" }} >
           <Text style={{ marginRight: 11, backgroundColor: "#eb4034", color: "black", borderRadius: 11, fontSize: 28, marginBottom: 20 }}> finfriend </Text>
